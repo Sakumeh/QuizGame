d60527f6271849d3bf6b846a0d0c7b67d04bd453
Sakumeh <SamehPro@outook.fr>
2024-04-06 20:32:32 +0200

    Fin du projet

--- main.swift
+++ main.swift
@@ -28,6 +28,8 @@ struct ScoreUtilisateur: Codable {
     let categorie: String
 }
 
+
+
 // Fonction pour charger les questions à partir d'un fichier JSON
 func chargerQuestions(from file: String) -> [Question]? {
     // Chemin du fichier JSON contenant les questions
@@ -43,6 +45,20 @@ func chargerQuestions(from file: String) -> [Question]? {
         return nil
     }
 }
+
+
+
+// Fonction pour demander à l'utilisateur son nom
+func getNomJoueur() -> String {
+    print("Entrez votre nom:")
+    if let nomJoueur = readLine() {
+        return nomJoueur
+    } else {
+        return "Joueur"
+    }
+}
+
+
 
 // Fonction pour permettre à l'utilisateur de sélectionner la difficulté du quiz
 func selectionDifficulte() -> Difficulte? {
@@ -71,6 +87,8 @@ func selectionDifficulte() -> Difficulte? {
     return difficulte
 }
 
+
+
 // Fonction pour charger les questions en fonction de la catégorie et de la difficulté sélectionnées
 func chargerQuestions(for categorie: String, difficulty: Difficulte) -> [Question]? {
     guard let allQuestions = chargerQuestions(from: "questions") else {
@@ -79,6 +97,8 @@ func chargerQuestions(for categorie: String, difficulty: Difficulte) -> [Questio
     let filteredQuestions = allQuestions.filter { $0.categorie == categorie && $0.difficulte == difficulty }
     return filteredQuestions.isEmpty ? nil : filteredQuestions
 }
+
+
 
 // Fonction pour mélanger les questions afin de rendre le quiz plus aléatoire
 func shuffleQuestions(_ questions: [Question]) -> [Question] {
@@ -156,3 +176,106 @@ func chargerScoreUtilisateurs() -> [ScoreUtilisateur]? {
 func afficherScoreFinal(_ score: Int) {
     print("Votre score final est de \(score) points.")
 }
+
+
+
+// Fonction pour permettre à l'utilisateur de sélectionner la catégorie du quiz
+func selectionCategorie() -> String? {
+    print("Veuillez choisir la catégorie :")
+    print("1 - Géographie")
+    print("2 - Naruto")
+    // Ajoutez d'autres catégories selon votre fichier JSON
+    
+    var categorie: String?
+    var isValidInput = false
+    
+    repeat {
+        if let input = readLine(), let choice = Int(input), (1...2).contains(choice) {
+            switch choice {
+            case 1: categorie = "Géographie"
+            case 2: categorie = "Naruto"
+            // Ajoutez d'autres cas pour d'autres catégories si nécessaire
+            default: break
+            }
+            isValidInput = true
+        } else {
+            print("Choix invalide. Veuillez entrer le numéro correspondant à la catégorie.")
+        }
+    } while !isValidInput
+    
+    return categorie
+}
+
+
+
+// Fonction principale pour démarrer le jeu
+func startGame() {
+    var jouerEncore = true
+    while jouerEncore {
+        print("1. Jouer")
+        print("2. Afficher les scores")
+        
+        if let choix = readLine(), let menuChoice = Int(choix) {
+            switch menuChoice {
+            case 1:
+                // Jouer au quiz
+                let nomJoueur = getNomJoueur()
+                guard let categorie = selectionCategorie() else {
+                    print("Catégorie invalide.")
+                    continue
+                }
+                
+                guard let difficulte = selectionDifficulte() else {
+                    print("Option invalide.")
+                    continue
+                }
+                
+                guard let questions = chargerQuestions(for: categorie, difficulty: difficulte) else {
+                    print("Impossible de charger les questions pour la catégorie et la difficulté sélectionnées.")
+                    continue
+                }
+                
+                let shuffledQuestions = shuffleQuestions(questions)
+                var score = 0
+                
+                for question in shuffledQuestions {
+                    if let reponseUtilisateur = getReponseUtilisateur(for: question), reponseUtilisateur == question.indexBonneRep {
+                        score += 1
+                        print("Correct!")
+                    } else {
+                        print("Dommage, c'était \(question.options[question.indexBonneRep])")
+                    }
+                }
+                
+                afficherScoreFinal(score)
+                saveScoreUtilisateur(nomJoueur: nomJoueur, score: score, difficulte: difficulte, categorie: categorie)
+                
+            case 2:
+                // Afficher les scores des utilisateurs
+                if let scores = chargerScoreUtilisateurs() {
+                    // Triez les scores en fonction du score décroissant
+                    let sortedScores = scores.sorted { $0.score > $1.score }
+                    print("Scores des joueurs:")
+                    for score in sortedScores {
+                        print("\(score.nomJoueur): \(score.score) (Difficulté: \(score.difficulte.rawValue), Catégorie: \(score.categorie))")
+                    }
+                } else {
+                    print("Aucun score disponible.")
+                }
+            default:
+                print("Choix invalide.")
+            }
+        } else {
+            print("Choix invalide.")
+        }
+        
+        // Demander à l'utilisateur s'il souhaite revenir au menu principal
+        print("Souhaitez-vous revenir au menu? (Oui/Non)")
+        if let choix = readLine(), choix.lowercased() == "non" {
+            jouerEncore = false
+        }
+    }
+}
+
+// Démarrer le jeu
+startGame()
